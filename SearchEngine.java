c6299719e37be3e08bc811e07c0d9c5fa15d4410
Wing <chanwingyin10@gmail.com>
2022-10-06 11:51:32 -0700

    Create SearchEngine.java

---
+++ SearchEngine.java
@@ -0,0 +1,53 @@
+import java.io.IOException;
+import java.net.URI;
+import java.util.*;
+
+class Handler implements URLHandler {
+    // The one bit of state on the server: a number that will be manipulated by
+    // various requests.
+    List<String> list = new ArrayList<String>();
+
+    public String handleRequest(URI url) {
+        if (url.getPath().equals("/")) {
+            return String.format("Hi!");
+        } else if (url.getPath().equals("/increment")) {
+
+            return String.format("Number incremented!");
+        } else {
+            System.out.println("Path: " + url.getPath());
+            if (url.getPath().contains("/add")) {
+                String[] parameters = url.getQuery().split("=");
+                if (parameters[0].equals("s")) {
+                    list.add(parameters[1]);
+                    return String.format("String added: %s!", parameters[1]);
+                }
+            } else if (url.getPath().contains("/search")) {
+                List<String> temp = new ArrayList<String>();
+                String[] parameters = url.getQuery().split("=");
+                if (parameters[0].equals("s")) {
+                    for (int i = 0; i < list.size(); i++) {
+                        if (list.get(i).contains(parameters[1])) {
+                            temp.add(list.get(i));
+                        }
+                    }
+                }
+                return String.format("Find Strings: %s", temp.toString().replace("[", "").replace("]", ""));
+            }
+            return "404 Not Found!";
+        }
+    }
+}
+
+class SearchEngine {
+    public static void main(String[] args) throws IOException {
+        if (args.length == 0) {
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new Handler());
+    }
+
+}
